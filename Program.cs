864e2a1283c62b913b4891ae153db0bc592b4a18
ZussiMane <igoryunja@gmail.com>
2024-03-16 13:59:31 +0300

    Добавил решение задачи

--- Program.cs
+++ Program.cs
@@ -5,3 +5,18 @@
 // 456 => [6 5 4]
 // 781 => [1 8 7]
 
+Console.WriteLine("Введите трёхзначное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[3];
+
+for (int i = 0; i < 3; i++) // 
+{
+    array[i] = number % 10; // получили последнее число (6)
+    number = number / 10; // получили второе число (45)
+    // и повторно заходим в цикл до того, пока не получим 4 
+}
+for (int i = 0; i < array.Length; i++) // вывод новго массива, array.Length уже привязана к длине новго массива
+{
+    Console.Write(array[i] + " ");
+}
